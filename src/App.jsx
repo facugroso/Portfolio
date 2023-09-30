0a485b1e3e4b8b314787b7a493a061e507e671f4
facugroso <110434971+facugroso@users.noreply.github.com>
2023-09-30 16:36:14 -0300

    estructura terminada
    falta el estilo y el contenido final

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Main from "./components/Main";
 import Work from "./components/Work";
 import WorkItem from "./components/WorkItem";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Work />
       <WorkItem />
       <Projects />
+      <Contact />
     </div>
   );
 }
