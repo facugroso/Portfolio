006f29d663f6e399dad7b1ebb25cb44508ef9410
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-02 16:14:17 -0300

    prueba img 100

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -6,7 +6,7 @@ const Main = () => {
     <div id="main">
       <img
         className="w-full h-screen object-cover object-left- scale-x-[-1.4]"
-        src="public/backgroundImage.jpg"
+        src="https://www.ups.com/assets/resources/webcontent/images/how-to-ship-laptop-G-1134966284-Q421.jpg"
         alt="bacround image"
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/12">
