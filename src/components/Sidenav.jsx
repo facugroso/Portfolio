1f06a0f5fa2220b86c353523230762292d2eb0d7
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-02 10:43:12 -0300

    fin por ahora

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -20,6 +20,7 @@ const Sidenav = () => {
       {nav ? (
         <div className="fixed w-full h-screen bg-white/600 flex flex-col justify-center items-center z-40">
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[40%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -27,6 +28,7 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -34,6 +36,7 @@ const Sidenav = () => {
             <span className="pl-4">Work</span>
           </a>
           <a
+            onClick={handleNav}
             href="#project"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -41,6 +44,7 @@ const Sidenav = () => {
             <span className="pl-4">Proyect</span>
           </a>
           <a
+            onClick={handleNav}
             href="#resume"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -52,7 +56,7 @@ const Sidenav = () => {
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <CiMail size={20} />
-            <span className="pl-4">Contact</span>
+            <span className="pl-4">para revisar este codigo</span>
           </a>
         </div>
       ) : (
