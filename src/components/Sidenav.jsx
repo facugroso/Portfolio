25830f6111aab7672a370a99409d1712c2cc276a
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-02 16:33:51 -0300

    img 112

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -6,7 +6,7 @@ const Main = () => {
     <div id="main">
       <img
         className="w-full h-screen object-cover object-left- scale-x-[-1.4]"
-        src="../public/backgroundImage.jpg"
+        src="../../public/backgroundImage.jpg"
         alt="bacround image"
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/12">
