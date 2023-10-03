7c4294ca736c5079aa9bfe82ffd94f4abc20fac2
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-02 21:32:54 -0300

    dsf

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -20,7 +20,7 @@ const Sidenav = () => {
           <a
             onClick={handleNav}
             href="#main"
-            className="w-[40%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
