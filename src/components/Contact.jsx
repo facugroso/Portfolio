0f26e569e4e6eb1ddfdbc499d7425ffd706bf4e0
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-05 20:02:26 -0300

    asd

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div
-      id="contact"
-      className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 color"
-    >
+    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
@@ -19,7 +16,7 @@ const Contact = () => {
           <div className="flex flex-col">
             <label className="uppercase text-sm py-2">Name</label>
             <input
-              className="border  rounded-lg p-3 flex border-gray-700 "
+              className="border-  rounded-lg p-3 flex border-gray-700 "
               type="text"
               name="name"
             />
