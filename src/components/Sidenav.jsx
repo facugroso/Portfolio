a17e3ca39a5f5f0fd70a4a96221ed439fd916dbd
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-05 13:30:31 -0300

    <csc

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -25,37 +25,6 @@ const Sidenav = () => {
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
           </a>
-          <a
-            onClick={handleNav}
-            href="#work"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <PiProjectorScreenChartLight size={20} />
-            <span className="pl-4">Work</span>
-          </a>
-          <a
-            onClick={handleNav}
-            href="#proyect"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <GoProjectSymlink size={20} />
-            <span className="pl-4">Proyect</span>
-          </a>
-          <a
-            onClick={handleNav}
-            href="#resume"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <BsPerson size={20} />
-            <span className="pl-4">Resume</span>
-          </a>
-          <a
-            href="#contact"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <CiMail size={20} />
-            <span className="pl-4">Contact</span>
-          </a>
         </div>
       ) : (
         <div className="md:block hidden fixed top-[25%] z-10">
@@ -97,3 +66,35 @@ const Sidenav = () => {
   );
 };
 export default Sidenav;
+
+// <a
+//             onClick={handleNav}
+//             href="#work"
+//             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+//           >
+//             <PiProjectorScreenChartLight size={20} />
+//             <span className="pl-4">Work</span>
+//           </a>
+//           <a
+//             onClick={handleNav}
+//             href="#proyect"
+//             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+//           >
+//             <GoProjectSymlink size={20} />
+//             <span className="pl-4">Proyect</span>
+//           </a>
+//           <a
+//             onClick={handleNav}
+//             href="#resume"
+//             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+//           >
+//             <BsPerson size={20} />
+//             <span className="pl-4">Resume</span>
+//           </a>
+//           <a
+//             href="#contact"
+//             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+//           >
+//             <CiMail size={20} />
+//             <span className="pl-4">Contact</span>
+//           </a>
