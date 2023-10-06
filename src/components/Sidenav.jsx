b19fa0ffd27db96fe751d7901aba5915bce42fca
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-06 18:23:44 -0300

    Ajustando direcciones

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
 import { PiProjectorScreenChartLight } from "react-icons/pi";
 import { GoProjectSymlink } from "react-icons/go";
-import { BsPerson } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
 const Sidenav = () => {
   const [nav, setNav] = useState(false);
@@ -30,16 +29,24 @@ const Sidenav = () => {
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
-            <AiOutlineHome size={20} />
+            <PiProjectorScreenChartLight size={20} />
             <span className="pl-4">Work</span>
           </a>
           <a
             onClick={handleNav}
-            href="#main"
+            href="#proyects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
-            <AiOutlineHome size={20} />
-            <span className="pl-4">Home</span>
+            <GoProjectSymlink size={20} />
+            <span className="pl-4">Proyects</span>
+          </a>
+          <a
+            onClick={handleNav}
+            href="#contact"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <CiMail size={20} />
+            <span className="pl-4">Contact</span>
           </a>
         </div>
       ) : (
@@ -58,16 +65,10 @@ const Sidenav = () => {
               <PiProjectorScreenChartLight size={20} />
             </a>
             <a
-              href="#proyect"
+              href="#proyects"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             >
               <GoProjectSymlink size={20} />
-            </a>
-            <a
-              href="#resume"
-              className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-            >
-              <BsPerson size={20} />
             </a>
             <a
               href="#contact"
