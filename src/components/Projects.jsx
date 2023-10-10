9210e2dd7b8ec68b214180e1e2f5c852df7e7e04
facugroso <110434971+facugroso@users.noreply.github.com>
2023-10-10 20:23:35 -0300

    ewsd

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,8 +9,7 @@ const Projects = () => {
         Projects
       </h1>
       <p className="text-center py-8">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum et id
-        sed repellendus
+        En estos 
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
         <ProjectItem img={hyperx} title="HYPERX" />
