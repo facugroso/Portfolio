c933d23c2b787d4945374e4e1c9d57ac4016e25d
facugroso <110434971+facugroso@users.noreply.github.com>
2024-02-09 14:29:38 -0300

    ajustes: bienvenida, cv, ubicación

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,26 @@
 import React from "react";
-import { TypeAnimation } from "react-type-animation";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 const Main = () => {
-  return (
-    <div id="main" className="relative flex">
+  return (<div>
+    <div id="main">
 
-      <div className="w-full sm:w-1/2 order-2">
+      
         <img
-          className="w-full min-h-screen object-cover"
+          className="mt-5 w-auto mx-auto"
           src="/img/photoWelcome.jpg"
           alt="background image"
         />
-      </div>
-      {/* <div className=" w-full sm:w-1/2 order-1 absolute top-0 left-0 bg-white/12 flex items-center"> */}
+      
+       
         <div className="max-w-2xl mx-auto text-center">
           <h1 className="text-4xl text-xl font-bold text-white-800">
-            Welcome, I'm Facundo Groso.
+            Welcome to my Portfolio. 
           </h1>
-          <h2 className="flex sm:text-3xl text-2xl pt-4 text-white-800">
-            <TypeAnimation
-              sequence={[
-                "Developer FullStack jr.",
-                1000,
-                "HTML5, CSS3, JavaScript and React.",
-                1000,
-                "Hack Academy Graduate.",
-                1000,
-                "I specialized in MERN.",
-                1000,
-              ]}
-              wrapper="span"
-              cursor={true}
-              repeat={Infinity}
-              style={{ fontSize: "0.8em", paddingLeft: "5px" }}
-            />
-          </h2>
+          <p className=" my-2 text-base font-normal text-stone-1000">
+            I´m Facundo Groso, an apassionate for the knowledge and the work. 
+          </p>
+
           <div className="flex justify-between items-end pt-6 max-w-[200px] w-full mx-auto">
             <a href="https://www.linkedin.com/in/facundogroso/">
               <FaLinkedinIn className="cursor-pointer" size={20} />
@@ -44,7 +29,7 @@ const Main = () => {
               <FaGithub className="cursor-pointer" size={20} />
             </a>
           </div>
-          <a href="public/img/CVFacundoGroso2023.pdf" download>
+          <a href="public/img/CV Eng - Facundo Groso - 2024.jpg" download>
             <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-3">
               <svg
                 className="fill-current w-4 h-4 mr-2"
@@ -57,9 +42,8 @@ const Main = () => {
             </button>
           </a>
         </div>
-      {/* </div> */}
 
-
+    </div>
     </div>
   );
 };
